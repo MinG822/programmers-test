f247e594ee0fab7a76c70baa71ce870a1036181a
MinG822 <aestas822@gmail.com>
2022-09-15 20:38:01 +0900

    passed big number

--- 42883_big_number.py
+++ 42883_big_number.py
@@ -1,49 +1,51 @@
+from collections import deque
+
+
 def solution(number, k):
-    answer = []
+    """
+    https://school.programmers.co.kr/questions/32364 참고
+    다음 위치 부터 마지막까지 비교할 필요없이,
+    바로 직전에 넣은 값과 비교하면 된다
+    마지막까지 비교해서, 다음 최대로 바로 이동하는 로직은,
+    최악의 경우, 그러니까 단조증가하는 경우 효율이 별로다
+    """
+    answer = deque([])
     ni = 0
     number = [int(n) for n in number]
-    # print(number)
+
+    if k == len(number) - 1:
+        return str(max(number))
+
     while ni < len(number):
-        # print("-----------")
-        # print(ni, number[ni], k, answer)
         if k == 0:  # 더 이상 숫자를 빼지 않아도 될 때
             answer.extend(number[ni:])
-            # print("case 1 no more drop")
             break
 
-        if len(number) - k == ni:  # 남은 숫자들을 전부 빼야할 때
-            # print("case 2 drop all")
-            break
-        if number[ni] == 9:
+        if not len(answer) or k == 0:
             answer.append(number[ni])
             ni += 1
             continue
-        # print("case 3, check and drop or not")
-        max_check_i = ni + k
-        # max_n, max_ni = number[ni], ni
-        # for nni in range(ni + 1, max_check_i + 1):
-        #     if number[nni] > max_n:
-        #         max_n = number[nni]
-        #         max_ni = nni
 
-        max_n, max_ni = number[max_check_i], max_check_i
-        for nni in range(max_check_i, ni - 1, -1):
-            if number[nni] < max_n:
-                continue
-            max_n = number[nni]
-            max_ni = nni
+        while len(answer) and answer[-1] < number[ni] and k > 0:
+            answer.pop()
+            k -= 1
 
-        # print("max n", max_n, "max_ni", max_ni)
-        answer.append(max_n)
-        k -= max_ni - ni
-        ni = max_ni + 1
-        # print("next one", ni, k, answer)
+        answer.append(number[ni])
 
-    return "".join([str(n) for n in answer])
+        ni += 1
+
+    answer_str = ""
+    answer_under = len(answer) - k
+    for ai, answer_num in enumerate(answer):
+        if ai < answer_under:
+            answer_str += str(answer_num)
+
+    return answer_str
 
 
 if __name__ == "__main__":
-    assert (solution("1234", 3)) == "4"
+    assert (solution("1234", 2)) == "34"
+    assert (solution("1234", 2)) == "34"
     assert (solution("1924", 2)) == "94"
     assert (solution("1231234", 3)) == "3234"
     assert (solution("4177252841", 4)) == "775841"
---
+++ 42883_big_number_performance_fail.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+
+def solution(number, k):
+    answer = deque([])
+    ni = 0
+    number = [int(n) for n in number]
+    # print(number)
+    if k == len(number) - 1:
+        return str(max(number))
+
+    while ni < len(number):
+        # print("-----------")
+        # print(ni, number[ni], k, answer)
+        if k == 0:  # 더 이상 숫자를 빼지 않아도 될 때
+            answer.extend(number[ni:])
+            # print("case 1 no more drop")
+            break
+
+        if len(number) - k == ni:  # 남은 숫자들을 전부 빼야할 때
+            # print("case 2 drop all")
+            break
+
+        if number[ni] == 9:
+            answer.append(number[ni])
+            ni += 1
+            continue
+
+        if len(number) - k - 1 == ni:
+            answer.append(max(number[ni:]))
+            break
+
+        # print("case 3, check and drop or not")
+        max_check_i = ni + k
+        # max_n, max_ni = number[ni], ni
+        # for nni in range(ni + 1, max_check_i + 1):
+        #     if number[nni] > max_n:
+        #         max_n = number[nni]
+        #         max_ni = nni
+
+        max_n, max_ni = number[max_check_i], max_check_i
+        for nni in range(max_check_i, ni - 1, -1):
+            if number[nni] < max_n:
+                continue
+            max_n = number[nni]
+            max_ni = nni
+
+        # print("max n", max_n, "max_ni", max_ni)
+        answer.append(max_n)
+        k -= max_ni - ni
+        ni = max_ni + 1
+        # print("next one", ni, k, answer)
+
+    return "".join([str(n) for n in answer])
+
+
+if __name__ == "__main__":
+    assert (solution("1234", 3)) == "4"
+    assert (solution("1924", 2)) == "94"
+    assert (solution("1231234", 3)) == "3234"
+    assert (solution("4177252841", 4)) == "775841"
+    assert (solution("1111", 2)) == "11"
